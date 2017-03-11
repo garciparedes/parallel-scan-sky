375eb622a132ca2e425a9b921a703362b045068a
Sergio Garcia <sergio@garciparedes.me>
2017-03-11 08:51:09 +0100

    Minor change removing loop

--- openmp/src/ScanSky_openmp.c
+++ openmp/src/ScanSky_openmp.c
@@ -136,8 +136,8 @@ int main (int argc, char* argv[])
 		nowait,\
 		schedule(dynamic, ((rows-1)*(columns-1))/omp_get_num_threads()), \
 		private(i,j,k)
-		for(i=1;i< rows-1; i++){
-			for(j=1;j< columns-1; j++){
+		for(i = 1; i < rows-1; i++){
+			for(j = 1; j < columns-1; j++){
 				// Si es 0 se trata del fondo y no lo computamos
 				if(matrixData[i*(columns)+j]!=0){
 					matrixResult[i*(columns)+j]=i*(columns)+j;
@@ -150,14 +150,6 @@ int main (int argc, char* argv[])
 					matrixResult[i*(columns)+j]=-1;
 				}
 			}
-		}
-
-
-		#pragma omp for \
-		nowait,\
-		schedule(static), \
-		private(i)
-		for(i=0;i< rows; i++){
 			matrixResult[i*(columns)]=-1;
 			matrixResult[i*(columns)+columns-1]=-1;
 		}
