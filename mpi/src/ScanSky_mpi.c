b0647b44df29d4a2e6c828b59356d14af22308b0
Sergio Garcia <sergio@garciparedes.me>
2017-03-22 15:20:16 +0100

    Added base code of MPI version

--- mpi/src/ScanSky_mpi.c
+++ mpi/src/ScanSky_mpi.c
@@ -5,7 +5,8 @@
 * Código secuencial base
 *
 * @author Ana Moretón Fernández
-* @version v1.2
+* @author Eduardo Rodríguez Gutiez
+* @version v1.3
 *
 * (c) 2017, Grupo Trasgo, Universidad de Valladolid
 */
@@ -15,13 +16,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include "cputils.h"
+#include <mpi.h>
 
 
 /* Substituir min por el operador */
 #define min(x,y)    ((x) < (y)? (x) : (y))
 
 /**
-* Funcion secuencial para la busqueda de mi bloque 
+* Funcion secuencial para la busqueda de mi bloque
 */
 int computation(int x, int y, int columns, int* matrixData, int *matrixResult, int *matrixResultCopy){
 	// Inicialmente cojo mi indice
@@ -49,9 +51,9 @@ int computation(int x, int y, int columns, int* matrixData, int *matrixResult, i
 		if(matrixResult[x*columns+y] == result){ return 0; }
 		// Si el indice cambia, actualizo matrix de resultados con el indice adecuado y retorno 1
 		else { matrixResult[x*columns+y]=result; return 1;}
-		
+
 	}
-	return 0; 
+	return 0;
 }
 
 /**
@@ -61,72 +63,80 @@ int main (int argc, char* argv[])
 {
 
 	/* 1. Leer argumento y declaraciones */
-	if (argc < 2) 	{ 		
-		printf("Uso: %s <imagen_a_procesar>\n", argv[0]); 		
-		return(EXIT_SUCCESS); 	
-	} 	
-	char* image_filename = argv[1]; 	
+	if (argc < 2) 	{
+		printf("Uso: %s <imagen_a_procesar>\n", argv[0]);
+		return(EXIT_SUCCESS);
+	}
+	char* image_filename = argv[1];
 
 	int rows=-1;
-	int columns =-1; 
-	int *matrixData=NULL; 
-	int *matrixResult=NULL; 
-	int *matrixResultCopy=NULL; 
+	int columns =-1;
+	int *matrixData=NULL;
+	int *matrixResult=NULL;
+	int *matrixResultCopy=NULL;
 	int numBlocks=-1;
+	int world_rank = -1;
+	int world_size = -1;
+	double t_ini;
+	int i,j;
 
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+	MPI_Comm_size (MPI_COMM_WORLD, &world_size);
 
+	if ( world_rank == 0 ) {
 
-	/* 2. Leer Fichero de entrada e inicializar datos */
+		/* 2. Leer Fichero de entrada e inicializar datos */
 
-	/* 2.1 Abrir fichero */
-	FILE *f = cp_abrir_fichero(image_filename);
+		/* 2.1 Abrir fichero */
+		FILE *f = cp_abrir_fichero(image_filename);
 
-	// Compruebo que no ha habido errores
-	if (f==NULL)
-	{
-	   perror ("Error al abrir fichero.txt");
-	   return -1;
-	}
-
-	/* 2.2 Leo valores del fichero */
-	int i,j,valor;
-	fscanf (f, "%d\n", &rows);
-	fscanf (f, "%d\n", &columns);
-	// Añado dos filas y dos columnas mas para los bordes
-	rows=rows+2;
-	columns = columns+2; 
-
-	/* 2.3 Reservo la memoria necesaria para la matriz de datos */
-	matrixData= (int *)malloc( rows*(columns) * sizeof(int) );
-	if ( (matrixData == NULL)   ) {
- 		perror ("Error reservando memoria");
-	   	return -1;
-	}
-
-	/* 2.4 Inicializo matrices */
-	for(i=0;i< rows; i++){
-		for(j=0;j< columns; j++){
-			matrixData[i*(columns)+j]=-1;
+		// Compruebo que no ha habido errores
+		if (f==NULL)
+		{
+			perror ("Error al abrir fichero.txt");
+			return -1;
 		}
-	}
-	/* 2.5 Relleno bordes de la matriz */
-	for(i=1;i<rows-1;i++){
-		matrixData[i*(columns)+0]=0;
-		matrixData[i*(columns)+columns-1]=0;
-	}
-	for(i=1;i<columns-1;i++){
-		matrixData[0*(columns)+i]=0;
-		matrixData[(columns-1)*(columns)+i]=0;
-	}
-	/* 2.6 Relleno la matriz con los datos del fichero */
-	for(i=1;i<rows-1;i++){
-		for(j=1;j<columns-1;j++){
-			fscanf (f, "%d\n", &matrixData[i*(columns)+j]);
-		}
-	}
-	fclose(f);
 
-	#ifdef WRITE
+		/* 2.2 Leo valores del fichero */
+		int valor;
+		fscanf (f, "%d\n", &rows);
+		fscanf (f, "%d\n", &columns);
+		// Añado dos filas y dos columnas mas para los bordes
+		rows=rows+2;
+		columns = columns+2;
+
+		/* 2.3 Reservo la memoria necesaria para la matriz de datos */
+		matrixData= (int *)malloc( rows*(columns) * sizeof(int) );
+		if ( (matrixData == NULL)   ) {
+			perror ("Error reservando memoria");
+			return -1;
+		}
+
+		/* 2.4 Inicializo matrices */
+		for(i=0;i< rows; i++){
+			for(j=0;j< columns; j++){
+				matrixData[i*(columns)+j]=-1;
+			}
+		}
+		/* 2.5 Relleno bordes de la matriz */
+		for(i=1;i<rows-1;i++){
+			matrixData[i*(columns)+0]=0;
+			matrixData[i*(columns)+columns-1]=0;
+		}
+		for(i=1;i<columns-1;i++){
+			matrixData[0*(columns)+i]=0;
+			matrixData[(rows-1)*(columns)+i]=0;
+		}
+		/* 2.6 Relleno la matriz con los datos del fichero */
+		for(i=1;i<rows-1;i++){
+			for(j=1;j<columns-1;j++){
+				fscanf (f, "%d\n", &matrixData[i*(columns)+j]);
+			}
+		}
+		fclose(f);
+
+		#ifdef WRITE
 		printf("Inicializacion \n");
 		for(i=0;i<rows;i++){
 			for(j=0;j<columns;j++){
@@ -134,61 +144,63 @@ int main (int argc, char* argv[])
 			}
 			printf("\n");
 		}
-	#endif
+		#endif
 
 
-	/* PUNTO DE INICIO MEDIDA DE TIEMPO */
-	double t_ini = cp_Wtime();
-
-//
-// EL CODIGO A PARALELIZAR COMIENZA AQUI
-//
-
-	/* 3. Etiquetado inicial */
-	matrixResult= (int *)malloc( (rows)*(columns) * sizeof(int) );
-	matrixResultCopy= (int *)malloc( (rows)*(columns) * sizeof(int) );
-	if ( (matrixResult == NULL)  || (matrixResultCopy == NULL)  ) {
- 		perror ("Error reservando memoria");
-	   	return -1;
+		/* PUNTO DE INICIO MEDIDA DE TIEMPO */
+		t_ini = cp_Wtime();
 	}
-	for(i=0;i< rows; i++){
-		for(j=0;j< columns; j++){
-			matrixResult[i*(columns)+j]=-1;
-			// Si es 0 se trata del fondo y no lo computamos
-			if(matrixData[i*(columns)+j]!=0){
-				matrixResult[i*(columns)+j]=i*(columns)+j;
-			}
+
+	//
+	// EL CODIGO A PARALELIZAR COMIENZA AQUI
+	//
+	if ( world_rank == 0 ) {
+
+		/* 3. Etiquetado inicial */
+		matrixResult= (int *)malloc( (rows)*(columns) * sizeof(int) );
+		matrixResultCopy= (int *)malloc( (rows)*(columns) * sizeof(int) );
+		if ( (matrixResult == NULL)  || (matrixResultCopy == NULL)  ) {
+			perror ("Error reservando memoria");
+			return -1;
 		}
-	}
-
-
-
-	/* 4. Computacion */
-	int t=0;
-	/* 4.1 Flag para ver si ha habido cambios y si se continua la ejecucion */
-	int flagCambio=1; 
-
-	/* 4.2 Busqueda de los bloques similiares */
-	for(t=0; flagCambio !=0; t++){
-		flagCambio=0; 
-
-		/* 4.2.1 Actualizacion copia */
-		for(i=1;i<rows-1;i++){
-			for(j=1;j<columns-1;j++){
-				if(matrixResult[i*(columns)+j]!=-1){
-					matrixResultCopy[i*(columns)+j]=matrixResult[i*(columns)+j];
+		for(i=0;i< rows; i++){
+			for(j=0;j< columns; j++){
+				matrixResult[i*(columns)+j]=-1;
+				// Si es 0 se trata del fondo y no lo computamos
+				if(matrixData[i*(columns)+j]!=0){
+					matrixResult[i*(columns)+j]=i*(columns)+j;
 				}
 			}
 		}
 
-		/* 4.2.2 Computo y detecto si ha habido cambios */
-		for(i=1;i<rows-1;i++){
-			for(j=1;j<columns-1;j++){
-				flagCambio= flagCambio+ computation(i,j,columns, matrixData, matrixResult, matrixResultCopy);
-			}
-		}
 
-		#ifdef DEBUG
+
+		/* 4. Computacion */
+		int t=0;
+		/* 4.1 Flag para ver si ha habido cambios y si se continua la ejecucion */
+		int flagCambio=1;
+
+		/* 4.2 Busqueda de los bloques similiares */
+		for(t=0; flagCambio !=0; t++){
+			flagCambio=0;
+
+			/* 4.2.1 Actualizacion copia */
+			for(i=1;i<rows-1;i++){
+				for(j=1;j<columns-1;j++){
+					if(matrixResult[i*(columns)+j]!=-1){
+						matrixResultCopy[i*(columns)+j]=matrixResult[i*(columns)+j];
+					}
+				}
+			}
+
+			/* 4.2.2 Computo y detecto si ha habido cambios */
+			for(i=1;i<rows-1;i++){
+				for(j=1;j<columns-1;j++){
+					flagCambio= flagCambio+ computation(i,j,columns, matrixData, matrixResult, matrixResultCopy);
+				}
+			}
+
+			#ifdef DEBUG
 			printf("\nResultados iter %d: \n", t);
 			for(i=0;i<rows;i++){
 				for(j=0;j<columns;j++){
@@ -196,33 +208,33 @@ int main (int argc, char* argv[])
 				}
 				printf("\n");
 			}
-		#endif
+			#endif
 
-	}
+		}
 
-	/* 4.3 Inicio cuenta del numero de bloques */
-	numBlocks=0;
-	for(i=1;i<rows-1;i++){
-		for(j=1;j<columns-1;j++){
-			if(matrixResult[i*columns+j] == i*columns+j) numBlocks++; 
+		/* 4.3 Inicio cuenta del numero de bloques */
+		numBlocks=0;
+		for(i=1;i<rows-1;i++){
+			for(j=1;j<columns-1;j++){
+				if(matrixResult[i*columns+j] == i*columns+j) numBlocks++;
+			}
 		}
 	}
 
-//
-// EL CODIGO A PARALELIZAR TERMINA AQUI
-//
+	//
+	// EL CODIGO A PARALELIZAR TERMINA AQUI
+	//
+	if ( world_rank == 0 ) {
 
-	/* PUNTO DE FINAL DE MEDIDA DE TIEMPO */
- 	double t_fin = cp_Wtime();
+		/* PUNTO DE FINAL DE MEDIDA DE TIEMPO */
+		double t_fin = cp_Wtime();
 
+		/* 5. Comprobación de resultados */
+		double t_total = (double)(t_fin - t_ini);
 
-
-	/* 5. Comprobación de resultados */
-  	double t_total = (double)(t_fin - t_ini);
-
-	printf("Result: %d\n", numBlocks);
-	printf("Time: %lf\n", t_total);
-	#ifdef WRITE
+		printf("Result: %d\n", numBlocks);
+		printf("Time: %lf\n", t_total);
+		#ifdef WRITE
 		printf("Resultado: \n");
 		for(i=0;i<rows;i++){
 			for(j=0;j<columns;j++){
@@ -230,11 +242,14 @@ int main (int argc, char* argv[])
 			}
 			printf("\n");
 		}
-	#endif
+		#endif
 
-	/* 6. Liberacion de memoria */
-	free(matrixData); 
-	free(matrixResult);
-	free(matrixResultCopy); 
+		/* 6. Liberacion de memoria */
+		free(matrixData);
+		free(matrixResult);
+		free(matrixResultCopy);
+	}
 
+	MPI_Finalize();
+	return 0;
 }
