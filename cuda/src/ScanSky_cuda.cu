3c6207c5b0579baea60318c229131705844beefa
Sergio Garcia <sergio@garciparedes.me>
2017-05-17 17:38:01 +0200

    Fixed warning

--- cuda/src/ScanSky_cuda.cu
+++ cuda/src/ScanSky_cuda.cu
@@ -31,7 +31,7 @@
 * code from: http://www.orangeowlsolutions.com/archives/613
 */
 #define gpuErrorCheck(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, char *file, int line, bool abort = true) {
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true) {
     if (code != cudaSuccess) {
 		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
 		if (abort) { exit(code); }
